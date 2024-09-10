f73f3872b6e5c082a2d9d52c9a484f4b6360484e
inluvwash <m2408044@edu.misis.ru>
2024-09-10 11:21:14 +0300

    Add files via upload

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,6 +8,7 @@ public class Program
     public static void Main()
     {
         Program program = new Program();
+        Console.WriteLine("Hello world");
 
         //program.Task_1_1();
         //program.Task_1_2();
